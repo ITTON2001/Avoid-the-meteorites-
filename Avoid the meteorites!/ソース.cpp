9970ea033c18bb72c2da81486c58d8243e901ea1
ITTON2001 <shinnosuke1529@gmail.com>
2023-10-23 21:20:43 +0900

    基本システム完成

--- Avoid the meteorites!/ソース.cpp
+++ Avoid the meteorites!/ソース.cpp
@@ -7,11 +7,11 @@
 #define SCREEN_WIDTH	(11)
 #define SCREEN_HEIGHT	(24)
 
-#define FPS			(10)
+#define FPS			(3)
 #define INTERVAL	(1000 / FPS)
 
 #define ATOMIC_COLUWN	(11)
-#define ATOMIC_ROW		(1)
+#define ATOMIC_ROW		(24)
 
 enum {
 	TILE_NONE,
@@ -63,20 +63,21 @@ PLAYER player;
 int atomicDirection;
 int atomicCount;
 
-
 //描画
 void DrawScreen() {
 	//隕石が移動する際に残る残像を消す
 	memset(screen, 0, sizeof screen);
 	//隕石を描画
-	for (int y = 0; y < ATOMIC_ROW; y++)
-		for (int x = 0; x < ATOMIC_COLUWN; x++)
+	for (int y = 0; y < ATOMIC_ROW; y++){
+		for (int x = 0; x < ATOMIC_COLUWN; x++) {
 			if (!atomic[y][x].isNone) {
 				screen[atomic[y][x].y][atomic[y][x].x] = TILE_ATOMIC;
 			}
 			else {
 				screen[atomic[y][x].y][atomic[y][x].x] = TILE_NONE;
 			}
+		}
+	}
 
 	//プレイヤーを描画
 	screen[player.y][player.x] = TILE_PLAYER;
@@ -86,6 +87,8 @@ void DrawScreen() {
 	for (int y = 0; y < SCREEN_HEIGHT; y++) {
 		for (int x = 0; x < SCREEN_WIDTH; x++)
 			printf("%s", tileAA[screen[y][x]]);
+			//printf("%d", screen[y][x]);
+			//printf("%d,%d", atomic[y][x].y,atomic[y][x].x);
 		printf("\n");
 	}
 }
@@ -95,21 +98,23 @@ void Init() {
 	// 現在の時間を使ってランダムなシードを設定
 	srand(time(NULL));
 
+	// ランダムな数値を得る
+	int randomNumber = rand() % SCREEN_WIDTH;
+
 	// 隕石の初期生成
-	int y = 0;
-	for (int x = 0; x < ATOMIC_COLUWN; x++) {
-		if (rand() % 2)
-			atomic[y][x].isNone = true;
-		else {
-			atomic[y][x].isNone = false;
+	for (int y = 0; y < SCREEN_HEIGHT; y++) {
+		for (int x = 0; x < SCREEN_WIDTH; x++) {
 			atomic[y][x].x = x;
 			atomic[y][x].y = y;
+			if (atomic[y][x].y == 0 && atomic[y][x].x != randomNumber)
+				atomic[y][x].isNone = false;
+			else
+				atomic[y][x].isNone = true;		
+
 		}
+		
 	}
 
-	//隕石の最初の向き
-	atomicDirection = DIRECTION_DOWN;
-	atomicCount = 0;
 
 	//プレイヤーの初期位置
 	player.x = SCREEN_WIDTH / 2;
@@ -130,19 +135,11 @@ bool AtomicIntersectPlayer() {
 	return false;
 }
 
-//隕石が画面外へ行ったら削除
-void AtomicOffScreen() {
-	for (int y = 0; y < ATOMIC_ROW; y++)
-		for (int x = 0; x < ATOMIC_COLUWN; x++)
-			if (atomic[y][x].y < 0) {
-				atomic[y][x].isNone = true;
-				printf("画面外");
-			}
-}
-
 //メイン
 int main() {
 	Init();
+	int check_count = 0;
+	int Atomatic_count = 0;
 
 	clock_t lastClock = clock();
 	while (1)
@@ -151,25 +148,45 @@ int main() {
 		if (nowClock >= lastClock + INTERVAL) {
 			lastClock = nowClock;
 
+			Atomatic_count += 1;
 
-			//隕石の挙動
-			for (int y = 0; y < ATOMIC_ROW; y++)
+			//隕石の挙動			
+			for (int y = ATOMIC_ROW - 1; y >= 0; y--) {
 				for (int x = 0; x < ATOMIC_COLUWN; x++) {
-
-					//atomic[y][x].x += directions[atomicDirection].x;
-					atomic[y][x].y += 1;
+					if (!atomic[y][x].isNone) {
+						check_count += 1;
+						atomic[y][x].isNone = true;
+						atomic[y + 1][x].isNone = false;
+					}
 				}
+			}
 
+			//5ターンに一回隕石を落とす
+			if (Atomatic_count == 5) {
+				Atomatic_count = 0;
+
+				// ランダムな数値を得る
+				int randomNumber = rand() % SCREEN_WIDTH;
+
+				for (int y = 0; y < SCREEN_HEIGHT; y++) {
+					for (int x = 0; x < SCREEN_WIDTH; x++) {
+						atomic[y][x].x = x;
+						atomic[y][x].y = y;
+						if (atomic[y][x].y == 0 && atomic[y][x].x != randomNumber)
+							atomic[y][x].isNone = false;
+					}
+				}
+			}
 
 			//隕石がプレイヤーに当たったらゲームオーバー
 			if (AtomicIntersectPlayer()) {
 				Init();
+				Atomatic_count = 0;
 				lastClock = clock();
 				continue;
 			}
 
-			AtomicOffScreen();
-
+			//AtomicOffScreen();
 			DrawScreen();
 		}
 
@@ -189,6 +206,13 @@ int main() {
 				player.x = 0;
 			if (player.x >= SCREEN_WIDTH)
 				player.x = SCREEN_WIDTH - 1;
+
+			//自分から隕石に当たってしまう
+			if (AtomicIntersectPlayer()) {
+				Atomatic_count = 0;
+				Init();
+				continue;
+			}
 
 			DrawScreen();
 		}
